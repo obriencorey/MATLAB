6e5640bb2e4c9bda8db2ae63613074a68ba686a4
obriencorey <cjobrien466@gmail.com>
2020-09-08 14:52:00 -0400

    geoplot test

---
+++ worldmap/Geoplot.m
@@ -0,0 +1,7 @@
+latSeattle = 47.62;
+lonSeattle = -122.33;
+latAnchorage = 61.20;
+lonAnchorage = -149.9;
+
+geoplot([latPittsburgh latAnchorage],[lonPittsburgh lonAnchorage],'g-*')
+geolimits([45 62],[-149 -123])
