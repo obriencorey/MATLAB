d8a3e0eed7a3ca68a726b3e485dc4da9ca58f650
obriencorey <cjobrien466@gmail.com>
2020-09-22 10:41:25 -0400

    added orbital height calulator

--- AAE/AAE25100 - Fall 2020/OrbitalHeight.m
+++ AAE/AAE25100 - Fall 2020/OrbitalHeight.m
@@ -7,9 +7,12 @@ Anom = input("Enter the truly anomaly(degrees): ");
 a = .5 * (Rp + Ra);
 e = (Ra - a) / a;
 p = a * (1 - e * e);
+mu = 3.986*10^5;
 
 R = p / (1 + e * cosd(Anom));
+V = sqrt(mu * (2/R - 1/a));
 
 fprintf("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n");
 fprintf("Your orbital radius is: %.3fkm. \nYour orbital height is %.3fkm\n",R,R-6371);
+fprintf("Your velocity is: %.3fkm/s\n",V);
 fprintf("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n");
