753e200c15535df2f42d8aab72d95f1aeda07314
obriencorey <cjobrien466@gmail.com>
2020-10-13 16:52:55 -0400

    plotting aspect ratio against range

--- AAE/AAE20401/recrusive.m
+++ AAE/AAE20401/recrusive.m
@@ -1,12 +1,14 @@
-xn = 0;
+xn = -1;
+xnplus1 = 6;
 upperlimit = 200;
 
 values = zeros(1,upperlimit);
 numbers = 1:upperlimit;
 for x = 0:upperlimit - 1
-    xnplus1 = 1 + sqrt(xn - 1);
+    xnplus2 = 1/3 * xnplus1 + 2/3 * xn;
     xn = xnplus1;
-    values(x + 1) = xn;
+    xnplus1 = xnplus2;
+    values(x + 1) = xnplus2;
 end
 
 hold on
---
+++ AAE/AAE25100 - Fall 2020/ARvsRange.m
@@ -0,0 +1,32 @@
+figure
+hold on
+xlabel('Range (nmi)');
+ylabel('Aspect Ratio');
+xlim([6000 9000])
+ylim([6 14]);
+set(gca,'FontName','Poppins');
+set(gca,'FontSize',12);
+title("Aspect Ratio - Range Trends");
+marksize = 15;
+
+%x is range, y is AR
+plot(6300,10.03,'.','MarkerSize',marksize);
+text(6300 - 50,10.03 + .4,'787-10');
+
+plot(7200,8.8,'.','MarkerSize',marksize);
+text(7200 - 50,8.8 + .4,'A330-900');
+
+plot(7350,11,'.','MarkerSize',marksize);
+text(7350 - 50,11 + .4,'787-8');
+
+plot(7650,11,'.','MarkerSize',marksize);
+text(7650 - 50,11 + .4,'787-9');
+
+plot(7500,9.96,'.','MarkerSize',marksize);
+text(7500 - 50,9.96 + .4,'777x-9');
+
+plot(8000,7.8,'.','MarkerSize',marksize);
+text(8000 - 50,7.8 + .4,'A380');
+
+grid on
+hold off
